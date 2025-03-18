661b0161dff6c09fafc2b94b2986c0a3fe21fe51
juliahnkim <110065113+juliahnkim@users.noreply.github.com>
2025-03-18 11:59:57 -0700

    Brainstorming structs and functions

--- DPHashmap.h
+++ DPHashmap.h
@@ -1,1 +1,37 @@
+#include <cmph.h>
+
+typedef struct pair {
+    char* key;
+    char* value;
+} pair_t;
+
+
+typedef struct PerfectHashTable {
+    int size;
+    int capacity;
+    cmph_t* mph;
+    pair_t** elements;
+} PHT;
+
+typedef int(*hashFunctionPointer)(char*, int);
+
+typedef struct DynamicPerfectHashTable {
+    int size;
+    int capacity;
+    hashFunctionPointer hashFunction;
+    PHT** tables;
+} DPHT;
+
+void insert();
+char* search();
+void delete_entry();
+int lookup(); // contains
+PHT* resize();
+DPHT* resize();
+void delete_table();
+int update();
+
+
+
+
 
