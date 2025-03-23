28f394105778719a8aa1b1ab1abeb1bc3a57cfb9
tysarna <tylersarna@gmail.com>
2025-03-23 15:21:45 -0700

    Update BigMap.h with most functions

--- BigMap.h
+++ BigMap.h
@@ -19,6 +19,13 @@ size_t _ht_default_hash(void* raw_key, size_t key_size) {
 	return hash;
 }
 
+size_t _ht_index_find(DPHT* ht, void* key){
+	size_t index;
+	index = ht->hashFunction(key, strlen(key));
+	index = index % ht->capacity;
+	return index;
+}
+
 typedef struct pair {
     char* key;
     char* value;
@@ -32,10 +39,65 @@ typedef struct DynamicPerfectHashTable {
     PHT** tables;
 } DPHT;
 
-void insert_dpht();
-char* search_dpht();
-void delete_entry_dpht();
-int lookup_dpht(); // contains
+DPHT* create_dpht(int buckets){
+	DPHT* dpht = (DPHT*)malloc(sizeof(DPHT));
+	dpht->size=0;
+	dpht->capacity=buckets;
+	dpht->largestPH=0;
+	dpht->hashFunction=&_ht_default_hash;
+	dpht->tables=(PHT**)malloc(sizeof(PHT*)*buckets);
+}
+
+void insert_dpht(DPHT* ht, char* key, char* value){
+	if(ht=NULL){
+		return;
+	}
+	size_t index = _ht_index_find(ht, key);
+	pair_t* newPair= (pair_t*)malloc(sizeof(pair_t));
+	if(newPair=NULL){
+		return;
+	}
+	newPair->key=(char*)malloc(strlen(key)*sizeof(char)+1);
+	newPair->value=(char*)malloc(strlen(value) * sizeof(char)+1);
+	strncpy(newPair->key, key, strlen(key)); 
+    	newPair->key[strlen(key)] = '\0'; 
+	strncpy(newPair->value, value, strlen(value));
+    	newPair->value[strlen(value)] = '\0';
+	
+	insert_pht(ht->tables[index], newPair);
+	if(ht->tables[index]->size > ht->largestPH){
+		ht->largestPH=ht->tables[index]->size
+	}
+	
+}
+
+char* search_dpht(DPHT* ht, char* key){
+	size_t index = _ht_index_find(ht, key);
+	return search_pht(ht->tables[index], key);
+}
+
+void delete_entry_dpht(DPHT* ht, char* key){
+	size_t index = _ht_index_find(ht, key);
+	return delete_entry_pht(ht->tables[index], key);
+}
+
+int lookup_dpht(PHT* ht, char* key){
+	size_t index = _ht_index_find(ht, key);
+	return lookup_pht(ht->tables[index], key);
+}
+
 DPHT* resize_dpht();
-void delete_table_dpht();
-int update_dpht();
+
+void delete_table_dpht(DPHT* ht){
+	int i;
+	for(i=0; i<ht->capacity; i++){
+		delete_table_pht(ht->tables[i]);
+	}
+	free(ht);
+}
+
+int update_dpht(DPHT* ht, char* key, char* value){
+	size_t index = _ht_index_find(ht, key);
+	return update_pht(ht->tables[index], key);
+
+}
