20cafa4c18403551cfcd4585215f0672c761225f
tysarna <tylersarna@gmail.com>
2025-03-23 13:43:34 -0700

    Update BigMap.h

--- BigMap.h
+++ BigMap.h
@@ -1,1 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include "PHT.h"
 
+typedef struct pair {
+    char* key;
+    char* value;
+} pair_t;
+
+typedef struct DynamicPerfectHashTable {
+    int size;
+    int capacity;
+    int largestPH;
+    hashFunctionPointer hashFunction;
+    PHT** tables;
+} DPHT;
+
+void insert_dph();
+char* search_dph();
+void delete_entry_dph();
+int lookup_dph(); // contains
+DPHT* resize_dph();
+void delete_table_dph();
+int update_dph();
